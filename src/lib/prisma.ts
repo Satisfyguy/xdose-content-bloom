7f6070d4749c59e4cb17e43d93abafb944483428
xvush <xvush310@gmail.com>
2025-06-14 20:41:07 +0200

    test auth

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from '@prisma/client';
 
+// Assurez-vous que le client Prisma est correctement généré et accessible
 const prisma = new PrismaClient();
 
 export default prisma; 
